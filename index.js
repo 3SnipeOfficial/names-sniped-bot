8e3ac1c72d6977bcaf628c389a0268f732b1beb2
lolwhenlifegivesyoulemons <62519659+lolwhenlifegivesyoulemons@users.noreply.github.com>
2020-09-04 21:16:39 -0600

    added dupe name protection!

--- index.js
+++ index.js
@@ -11,47 +11,51 @@ bot.on("ready", () => {
 bot.on("messageCreate", (msg) => {
     if(msg.channel.id == 751301214175625216) {
     	msg.delete('names-sniped-bot: Deleted automatically!');
-    	let yes = false;
-    	(async () => {
-		  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-		  const page = await browser.newPage();
-		  await page.goto(`https://namemc.com/profile/${msg.content}.100`);
-		  const cookies = [{
-		  	'name': 'theme',
-		  	'value': 'dark'
-		  }];
-		  await page.setCookie(...cookies);
-		  await page.reload()
-		  await page.click('[data-original-title=Discord]');
-		  await page.screenshot({path: 'screenshot.png'});
-		  const element = await page.$('.text-md-left');
-		  let text = await page.evaluate(element => element.innerHTML.split('content="')[1].split('"')[0], element)
-		  	.catch(()=>{browser.close()})
-		  if (text.includes('3snipe')) {
-		  	fs.readFile('./screenshot.png', function read(err, data) {
-			    if (err) {
-			        throw err;
-			    }
-			    content = new Buffer(data);
+    	bot.getMessages("736993036319588362", 1000)
+			.then((messages)=>{
+				if (messages.filter((a)=>a.content.toLowerCase().includes(msg.content.toLowerCase()))[0]) return msg.author.getDMChannel().then((user)=>{user.createMessage('You cannot submit an already submitted name.')}); else {
+					let yes = false;
+			    	(async () => {
+					  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+					  const page = await browser.newPage();
+					  await page.goto(`https://namemc.com/profile/${msg.content}.100`);
+					  const cookies = [{
+					  	'name': 'theme',
+					  	'value': 'dark'
+					  }];
+					  await page.setCookie(...cookies);
+					  await page.reload()
+					  await page.click('[data-original-title=Discord]');
+					  await page.screenshot({path: 'screenshot.png'});
+					  const element = await page.$('.text-md-left');
+					  let text = await page.evaluate(element => element.innerHTML.split('content="')[1].split('"')[0], element)
+					  if (text.includes('3snipe')) {
+					  	fs.readFile('./screenshot.png', function read(err, data) {
+						    if (err) {
+						        throw err;
+						    }
+						    content = new Buffer(data);
 
-			    embed = {embed: {
-		    		title: msg.content,
-		    		description: `${msg.author.mention} (${msg.author.id}) has submitted a name. Please identify if this name abides the rules.`
-		    		}
-		    	};
-		        bot.createMessage("751477536625786971", embed, {
-		        	file: content,
-  					name: msg.content+".png"
-		        })
-		        	.then((msg)=>{
-		        		msg.addReaction('👍');
-		        		msg.addReaction('👎');
-		        		msg.addReaction('🚫');
-		        	})
-			});
-		  }
-		  await browser.close();
-		})();
+						    embed = {embed: {
+					    		title: msg.content,
+					    		description: `${msg.author.mention} (${msg.author.id}) has submitted a name. Please identify if this name abides the rules.`
+					    		}
+					    	};
+					        bot.createMessage("751477536625786971", embed, {
+					        	file: content,
+			  					name: msg.content+".png"
+					        })
+					        	.then((msg)=>{
+					        		msg.addReaction('👍');
+					        		msg.addReaction('👎');
+					        		msg.addReaction('🚫');
+					        	})
+						});
+					  }
+					  await browser.close();
+					})();
+				}
+			})
    	}
 });
 bot.on("messageReactionAdd", (message, emoji, userId)=>{
@@ -61,7 +65,7 @@ bot.on("messageReactionAdd", (message, emoji, userId)=>{
 			bot.createMessage("736993036319588362", `\`${message.embeds[0].title}\` by ${message.embeds[0].description.split(' ')[0]}`, {file: content, name: message.embeds[0].title+".png"})
 			message.delete();
 			bot.createMessage(message.channel.id, "Sent successfully! A DM has been sent to "+message.embeds[0].description.split(' ')[0]+".");
-			bot.getDMChannel(bot.users.get(message.embeds[0].description.split('>')[0].split('<@')[1]).id)
+			bot.getDMChannel(message.embeds[0].description.split('>')[0].split('<@')[1])
 				.then((user)=>{	
 					user.createMessage(`Your submission for ${message.embeds[0].title} has been approved!`)
 						.catch((e)=>{
@@ -72,7 +76,7 @@ bot.on("messageReactionAdd", (message, emoji, userId)=>{
 		} else if (emoji.name=="👎") {
 			message.delete();
 			bot.createMessage(message.channel.id, `Denied ${message.embeds[0].description.split(' ')[0]}. A DM has been sent to them.`);
-			bot.getDMChannel(bot.users.get(message.embeds[0].description.split('>')[0].split('<@')[1]).id)
+			bot.getDMChannel(message.embeds[0].description.split('>')[0].split('<@')[1])
 				.then((user)=>{
 					user.createMessage(`Your submission for ${message.embeds[0].title} has been denied.`)
 						.catch((e)=>{
@@ -83,9 +87,9 @@ bot.on("messageReactionAdd", (message, emoji, userId)=>{
 		} else if (emoji.name=="🚫") {
 			message.delete();
 			bot.createMessage(message.channel.id, `Blocked ${message.embeds[0].description.split(' ')[0]} from posting anymore submissions. A DM has been sent to them.`)
-			bot.getDMChannel(bot.users.get(message.embeds[0].description.split('>')[0].split('<@')[1]).id)
+			bot.getDMChannel(message.embeds[0].description.split('>')[0].split('<@')[1])
 				.then((user)=>{
-					user.createMessage('You have been banned from posting in <#751301214175625216> due to Spam/Abuse.')
+					user.createMessage('You have been banned from posting in <#751301214175625216> due to Spam or Abuse.')
 						.catch((e)=>{
 							bot.createMessage(message.channel.id, "Direct messages are off for "+message.embeds[0].description.split(' ')[0]+".");
 						})
